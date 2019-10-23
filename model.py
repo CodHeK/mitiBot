6b75b7be6513349898007b4032a985f3779cef9e
CodHeK <gaganganapathyas@yahoo.in>
2019-10-23 20:06:29 +0530

    added copy, modularized code

--- model.py
+++ model.py
@@ -8,213 +8,202 @@ from sklearn.linear_model import LogisticRegression
 
 pp = pprint.PrettyPrinter(indent=4)
 
-# GLOBAL VALUES
-graph = {}
-nodes = {}
-node_map = {}
 
-# FEATURES
-id_count = {}
-od_count = {}
-idw_count = {}
-odw_count = {}
-lcc_count = {}
-
-f = {}
-fvecs = []
-
-def read(filename):
-    with open(filename, 'r') as file:
-        content = file.readlines()
-    file.close()
-    return content
+class Build:
+    def __init__(self, data):
+        self.data = data
+        self.graph = {}
+        self.nodes = {}
+        self.node_map = {}
+        self.id_count = {}
+        self.od_count = {}
+        self.idw_count = {}
+        self.odw_count = {}
+        self.lcc_count = {}
+        self.f = {}
+        self.fvecs = []
 
 
-'''
-    Flow ingestion
-'''
-def extract(x):
-    if len(x.split(',')) == 15:
-        sip = x.split(',')[3]
-        dip = x.split(',')[6]
+    '''
+        Flow ingestion
+    '''
+    def extract(self, x):
+        if len(x.split(',')) == 15:
+            sip = x.split(',')[3]
+            dip = x.split(',')[6]
 
-        nodes[sip] = 1
+            self.nodes[sip] = 1
 
-        # if sip not in nodes:
-        #     nodes[sip] = 1
-        # if dip not in nodes:
-        #     nodes[dip] = 1
+            byteSize = int(x.split(',')[12])/int(x.split(',')[11])
+            srcpkts = int(x.split(',')[13])/byteSize
+            dstpkts = int(x.split(',')[11]) - srcpkts
+            flow = x.split(',')[14]
 
-        byteSize = int(x.split(',')[12])/int(x.split(',')[11])
-        srcpkts = int(x.split(',')[13])/byteSize
-        dstpkts = int(x.split(',')[11]) - srcpkts
-        flow = x.split(',')[14]
+            if len(re.findall("Botnet", str(flow))) > 0:
+                self.node_map[sip] = 1
+            else:
+                self.node_map[sip] = 0
 
-        if len(re.findall("Botnet", str(flow))) > 0:
-            node_map[sip] = 1
+            # SIP -> DIP
+            if sip not in self.graph:
+                self.graph[sip] = {}
+
+            if dip not in self.graph[sip]:
+                self.graph[sip][dip] = (srcpkts, (srcpkts, dstpkts))
+            else:
+                srcpktsPrev = self.graph[sip][dip][0]
+                self.graph[sip][dip] = (srcpkts + srcpktsPrev, (srcpkts, dstpkts))
+
+            if dip in self.graph:
+                if sip in self.graph[dip]:
+                    srcpktsX = self.graph[sip][dip][0]
+                    dstpktsY = self.graph[dip][sip][1][1]
+                    self.graph[sip][dip] = (srcpktsX + dstpktsY, (srcpkts, dstpkts))
+
+
+            # DIP -> SIP
+            if dip not in self.graph:
+                self.graph[dip] = {}
+
+            if sip not in self.graph[dip]:
+                self.graph[dip][sip] = (dstpkts, (srcpkts, dstpkts))
+            else:
+                dstpktsPrev = self.graph[dip][sip][0]
+                self.graph[dip][sip] = (dstpkts + dstpktsPrev, (srcpkts, dstpkts))
+
+            if sip in self.graph:
+                if dip in self.graph[sip]:
+                    dstpktsX = self.graph[dip][sip][0]
+                    srcpktsY = self.graph[sip][dip][1][1]
+                    self.graph[dip][sip] = (dstpktsX + srcpktsY, (srcpkts, dstpkts))
+
+            return (sip, dip)
+
+
+    '''
+        F-Norm Implementation using D = 1
+    '''
+    def normalize(self, fn, node):
+        N = 0
+        sumF = np.array([0, 0, 0, 0, 0]).astype('float64')
+        for n in self.nodes:
+            if n != node:
+                if node in self.graph:
+                    if n in self.graph[node]:
+                        N += 1
+                        sumF += np.array(self.f[n][0]).astype('float64')
+
+        if N > 0:
+            u = sumF/float(N)
+
+            for idx, ui in enumerate(u):
+                if ui == 0.0:
+                    ui += 0.01
+                fn[idx] = fn[idx]/float(ui)
+
+        return fn
+
+
+    '''
+        Graph Transform and extracted feature storage
+    '''
+    def preprocess(self):
+        for line in self.data:
+            self.extract(line)
+
+        print("Graph built!")
+
+        # FOR EACH NODE CALCULATE THE FEATURE TUPLE [ F0, F1, F2, F3, F4 ]
+
+        print(len(self.nodes))
+        for node in self.nodes:
+            self.f[node] = [ [ self.ID(node), self.OD(node), self.IDW(node), self.ODW(node), self.LCC(node) ], self.node_map[node] ]
+
+        print("Feature Extraction done!")
+
+        # NORMALIZE
+
+        for node in self.nodes:
+            self.f[node][0] = self.normalize(self.f[node][0], node)
+            self.fvecs.append(self.f[node][0])
+
+        with open('./saved/fvecs.json', 'w') as fv:
+            json.dump(self.fvecs, fv, indent=4)
+
+        with open('./saved/f.json', 'w') as feat:
+            json.dump(self.f, feat, indent=4)
+
+        print("Normalizing Done!")
+
+
+    '''
+        Feature Extraction
+    '''
+    # IN-DEGREE (ID)
+    def ID(self, node):
+        c = 0
+        for n in self.nodes:
+            if n != node:
+                if n in self.graph:
+                    if node in self.graph[n]:
+                        c += 1
+
+        return c
+
+    # OUT-DEGREE (OD)
+    def OD(self, node):
+        c = 0
+        for n in self.nodes:
+            if n != node:
+                if node in self.graph:
+                    if n in self.graph[node]:
+                        c += 1
+        return c
+
+    # IN-DEGREE WEIGHT (IDW)
+    def IDW(self, node):
+        c = 0
+        for n in self.nodes:
+            if n != node:
+                if n in self.graph:
+                    if node in self.graph[n]:
+                        c += self.graph[n][node][0]
+        return c
+
+    # OUT-DEGREE WIGHT (ODW)
+    def ODW(self, node):
+        c = 0
+        for n in self.nodes:
+            if n != node:
+                if node in self.graph:
+                    if n in self.graph[node]:
+                        c += self.graph[node][n][0]
+        return c
+
+    # LOCAL CLUSTERING COEFFICIENT (LCC)
+    def LCC(self, node):
+        N = 0
+        NgNodes = []
+        for n in self.nodes:
+            if n != node:
+                if node in self.graph:
+                    if n in self.graph[node]:
+                        N += 1
+                        NgNodes.append(n)
+
+        F = 0
+        for j in NgNodes:
+            for k in NgNodes:
+                if j != k:
+                    if j in self.graph:
+                        if k in self.graph[j]:
+                            F += 1
+
+        if N > 1:
+            return F/float(N*(N-1))
         else:
-            node_map[sip] = 0
+            return 0.0
 
-        # SIP -> DIP
-        if sip not in graph:
-            graph[sip] = {}
-
-        if dip not in graph[sip]:
-            graph[sip][dip] = (srcpkts, (srcpkts, dstpkts))
-        else:
-            srcpktsPrev = graph[sip][dip][0]
-            graph[sip][dip] = (srcpkts + srcpktsPrev, (srcpkts, dstpkts))
-
-        if dip in graph:
-            if sip in graph[dip]:
-                srcpktsX = graph[sip][dip][0]
-                dstpktsY = graph[dip][sip][1][1]
-                graph[sip][dip] = (srcpktsX + dstpktsY, (srcpkts, dstpkts))
-
-
-        # DIP -> SIP
-        if dip not in graph:
-            graph[dip] = {}
-
-        if sip not in graph[dip]:
-            graph[dip][sip] = (dstpkts, (srcpkts, dstpkts))
-        else:
-            dstpktsPrev = graph[dip][sip][0]
-            graph[dip][sip] = (dstpkts + dstpktsPrev, (srcpkts, dstpkts))
-
-        if sip in graph:
-            if dip in graph[sip]:
-                dstpktsX = graph[dip][sip][0]
-                srcpktsY = graph[sip][dip][1][1]
-                graph[dip][sip] = (dstpktsX + srcpktsY, (srcpkts, dstpkts))
-
-        return (sip, dip)
-
-
-'''
-    Feature Extraction
-'''
-
-# IN-DEGREE (ID)
-def ID(node):
-    c = 0
-    for n in nodes:
-        if n != node:
-            if n in graph:
-                if node in graph[n]:
-                    c += 1
-
-    return c
-
-# OUT-DEGREE (OD)
-def OD(node):
-    c = 0
-    for n in nodes:
-        if n != node:
-            if node in graph:
-                if n in graph[node]:
-                    c += 1
-    return c
-
-# IN-DEGREE WEIGHT (IDW)
-def IDW(node):
-    c = 0
-    for n in nodes:
-        if n != node:
-            if n in graph:
-                if node in graph[n]:
-                    c += graph[n][node][0]
-    return c
-
-# OUT-DEGREE WIGHT (ODW)
-def ODW(node):
-    c = 0
-    for n in nodes:
-        if n != node:
-            if node in graph:
-                if n in graph[node]:
-                    c += graph[node][n][0]
-    return c
-
-# LOCAL CLUSTERING COEFFICIENT (LCC)
-def LCC(node):
-    N = 0
-    NgNodes = []
-    for n in nodes:
-        if n != node:
-            if node in graph:
-                if n in graph[node]:
-                    N += 1
-                    NgNodes.append(n)
-
-    F = 0
-    for j in NgNodes:
-        for k in NgNodes:
-            if j != k:
-                if j in graph:
-                    if k in graph[j]:
-                        F += 1
-
-    if N > 1:
-        return F/float(N*(N-1))
-    else:
-        return 0.0
-
-
-'''
-    F-Norm Implementation using D = 1
-'''
-def normalize(fn, node):
-    N = 0
-    sumF = np.array([0, 0, 0, 0, 0]).astype('float64')
-    for n in nodes:
-        if n != node:
-            if node in graph:
-                if n in graph[node]:
-                    N += 1
-                    sumF += np.array(f[n][0]).astype('float64')
-
-    if N > 0:
-        u = sumF/float(N)
-
-        for idx, ui in enumerate(u):
-            if ui == 0.0:
-                ui += 0.01
-            fn[idx] = fn[idx]/float(ui)
-
-    return fn
-
-
-'''
-    Graph Transform and extracted feature storage
-'''
-def preprocess(content):
-    for line in content:
-        extract(line)
-
-    print("Graph built!")
-
-    # FOR EACH NODE CALCULATE THE FEATURE TUPLE [ F0, F1, F2, F3, F4 ]
-
-    print(len(nodes))
-    for node in nodes:
-        f[node] = [ [ ID(node), OD(node), IDW(node), ODW(node), LCC(node) ], node_map[node] ]
-
-    print("Feature Extraction done!")
-
-    # NORMALIZE
-
-    for node in nodes:
-        f[node][0] = normalize(f[node][0], node)
-        fvecs.append(f[node][0])
-
-    with open('./saved/fvecs.json', 'w') as fv:
-        json.dump(fvecs, fv, indent=4)
-
-    with open('./saved/f.json', 'w') as feat:
-        json.dump(f, feat, indent=4)
-
-    print("Normalizing Done!")
 
 
 '''
@@ -269,34 +258,18 @@ def train_p2():
 
 
 def test():
-    outputs = {}
-    dbscan = pickle.load(open("./saved/dbscan.pkl", "rb"))
+    LR = pickle.load(open("./saved/LR.pkl", "rb"))
 
-    for lb in dbscan.labels_:
-        if lb not in outputs:
-            outputs[lb] = 1
-        else:
-            outputs[lb] += 1
+    with open("./saved/f.json", "r") as feat:
+        sf = json.load(feat)
 
-    c = 0
-    for i in outputs:
-        if i != 0:
-            c += outputs[i]
+    acc = 0
+    for i, item in enumerate(sf):
+        if str(LR.predict([ sf[item][0] ])[0]) == str(sf[item][1]):
+            acc += 1
 
-    print(c)
 
-    # PLOT CLUSTER SIZES
-
-    # x = [ v for v in outputs ]
-    #
-    # x.sort()
-    #
-    # y = [ outputs[i] for i in x ]
-    #
-    # plt.plot(x, y, 'ro')
-    # plt.show()
-
-    print(outputs)
+    print("Accuracy: " + str((acc*100)/float(len(sf))) + " %")
 
 
 def mod(content):
@@ -328,11 +301,18 @@ def mod(content):
     return (train, test)
 
 
+def read(filename):
+    with open(filename, 'r') as file:
+        content = file.readlines()
+    file.close()
+    return content
+
 
 if __name__ == '__main__':
     # FLAGS
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--train", help="trains model", action="store_true")
     parser.add_argument("--phase1", help="trains model", action="store_true")
     parser.add_argument("--phase2", help="trains model", action="store_true")
     parser.add_argument("--test", help="evaluates model", action="store_true")
@@ -351,11 +331,22 @@ if __name__ == '__main__':
 
     Train, Test = mod(content[1:])
 
+    if args.train:
+        b = Build(Train)
+        b.preprocess()
+
+        print("Done pre-processing on Train set!")
+
+        train_p1()
+
+        train_p2()
+
     if args.phase1:
         # PRE-PROCESS THE TRAINING DATASET & UNSUPERVISED LEARNING
-        preprocess(Train)
+        b = Build(Train)
+        b.preprocess()
 
-        print("Done pre-processing!")
+        print("Done pre-processing on Train set!")
 
         train_p1()
 
@@ -365,6 +356,11 @@ if __name__ == '__main__':
         train_p2()
 
     if args.test:
+        t = Build(Test)
+        t.preprocess()
+
+        print("Done pre-processing on Test set!")
+
         test()
 
     #########################################
